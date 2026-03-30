b08c5c7a58c7d6c4d2967d43da950333ca9f53e0
Geo De Guzman <22-70720@g.batstate-u.edu.ph>
2026-03-30 14:35:15 +0800

    march 30 2:35 update

--- inc/footer.php
+++ inc/footer.php
@@ -4,14 +4,17 @@
         <div class="col-lg-4 p-4">
             <h4 class="fw-bold fs-4 mb-2 text-danger">BSU LAMS</h4>
             <p>
-                Laboratory Asset Management System for the Hot Kitchen, Cold Kitchen, Food & Beverage Services, and Laundry facilities of Batangas State University.
+                Laboratory Asset Management System for the Hot Kitchen, Cold Kitchen, Food & Beverage Services, and
+                Laundry facilities of Batangas State University.
             </p>
         </div>
         <div class="col-lg-4 p-4">
             <h5 class="mb-3 text-dark">Links</h5>
             <a href="<?= BASE_URL ?>/index.php" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
-            <a href="<?= BASE_URL ?>/reserve.php" class="d-inline-block mb-2 text-dark text-decoration-none">Make Reservation</a><br>
-            <a href="<?= BASE_URL ?>/my_reservations.php" class="d-inline-block mb-2 text-dark text-decoration-none">My Reservations</a><br>
+            <a href="<?= BASE_URL ?>/reserve.php" class="d-inline-block mb-2 text-dark text-decoration-none">Make
+                Reservation</a><br>
+            <a href="<?= BASE_URL ?>/my_reservations.php" class="d-inline-block mb-2 text-dark text-decoration-none">My
+                Reservations</a><br>
         </div>
         <div class="col-lg-4 p-4">
             <h5 class="mb-3 text-dark">Contact Us</h5>
@@ -26,4 +29,5 @@
 
 <script src="<?= BASE_URL ?>/assets/js/main.js?v=<?= time() ?>"></script>
 </body>
+
 </html>
